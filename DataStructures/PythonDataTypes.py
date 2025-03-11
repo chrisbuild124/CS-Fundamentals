262d563b451e303822a19ef2dc7f780e7cce543d
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2025-03-11 14:27:44 -0400

    Update PythonDataTypes.py

--- DataStructures/PythonDataTypes.py
+++ DataStructures/PythonDataTypes.py
@@ -1,1 +1,25 @@
+# @chrisbuild124
+# These are Python built in data Structures and notes.
+# This includes: Dictionary, Sets, Lists, and Tuples
+# --------------------------------------Description--------------------------------------
 
+def main():
+    """
+    Description: This is the main function for the program.
+    """
+    bar_number, time_interval = input_bubble_sort()
+    random_list = generate_random_list(bar_number)
+    bubble_sort(random_list, time_interval)
+
+
+# Input the bar number. Must be an integer from 1-50 inclusive.
+def input_bubble_sort():
+    """
+    Description: This is the main function for the program.
+    params:      Void.
+    :returns:    The number of different numbers to sort.
+    """
+
+
+if __name__ == '__main__':
+    main()
