38bf5d7ca892641512e6e78ca88100d441057a2a
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2025-11-17 16:14:28 -0500

    Update usingIndexAsValue.py

--- Algorithms/arrays/usingIndexAsValue.py
+++ Algorithms/arrays/usingIndexAsValue.py
@@ -1,1 +1,15 @@
+# Trick: Use the index in the array as the letter's value
+# This can be applied to many things (not just letters) - for example, bucket sort
+# Leetcode link: https://leetcode.com/problems/group-anagrams/
 
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashMap = defaultdict(list)
+        for string in strs:     
+            checkList = [0] * 26
+            for c in string:
+                checkList[ord(c) - ord('a')] += 1
+            hashMap[tuple(checkList)].append(string)
+        return list(hashMap.values())
