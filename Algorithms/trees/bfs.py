8b02860d4b783f4bb2d0143c1569d50ecf7b03ea
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2025-11-19 17:40:41 -0500

    Add BFS implementation for maximum depth calculation
    Implement BFS to find maximum depth of binary tree.

--- Algorithms/trees/bfs.py
+++ Algorithms/trees/bfs.py
@@ -1,1 +1,21 @@
+# Breadth First Search Recursive Version: Maximum Depth of Binary Tree
+# Link: https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
 
+# This is an implimentation of bfs on the maximum depth of binary tree problem.
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        q = deque()
+        if root:
+            q.append(root)
+
+        level = 0
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
