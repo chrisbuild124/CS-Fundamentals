19d8f72d43586579c0ca2aa921faa7dae6cabe8b
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2025-12-17 22:29:05 -0500

    Add Hierholzer's algorithm for itinerary reconstruction
    Implement Hierholzer's algorithm to find an Eulerian path in a directed graph, specifically for reconstructing an itinerary based on given tickets.

---
+++ Algorithms/graphs/Eulerianpath/HierholzerAlg.py
@@ -0,0 +1,23 @@
+# Hierholzer Algorithm - it uses a Eulerian path to search through and find the path.
+# It works in ElogE in this problem but it's very specific to this problem because
+# there are many solutions but the solution desired is ordered, so the algorithm
+# can search directly to find the correct answer. 
+
+# Link: https://leetcode.com/problems/reconstruct-itinerary
+
+# Since this needs to be in lexigraphical order, after sorting, each dfs call is 
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        adj = defaultdict(list)
+        for src, dst in sorted(tickets)[::-1]:
+            adj[src].append(dst)
+
+        res = []
+        def dfs(src):
+            while adj[src]:
+                dst = adj[src].pop()
+                dfs(dst)
+            res.append(src)
+
+        dfs('JFK')
+        return res[::-1]
