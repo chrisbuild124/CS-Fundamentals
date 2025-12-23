ec8d706549a1592b7567a79b347b1a3bd68d8ef5
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2025-12-22 23:22:37 -0500

    Add Bellman Ford optimized algorithm for network delay
    Implemented the Bellman Ford optimized algorithm for calculating network delay time. Added detailed comments and notes for clarity.

---
+++ Algorithms/graphs/bellmanFord/ShortestPathFastestAlgorithm.py
@@ -0,0 +1,27 @@
+# Network Delay Time
+# Bellman Ford Optimized
+# Only optimize edges that are improved. 
+
+# NOTE: Solution was taken directly from neetcode 
+# O(E + V) average, O(E*V) worst
+class Solution:
+    def networkDelayTime(self, times, n, k):
+        adj = defaultdict(list)
+        for u, v, w in times:
+            adj[u].append((v, w))
+
+        dist = {node: float("inf") for node in range(1, n + 1)}
+        q = deque([(k, 0)])
+        dist[k] = 0
+
+        while q:
+            node, time = q.popleft()
+            if dist[node] < time:
+                continue
+            for nei, w in adj[node]:
+                if time + w < dist[nei]:
+                    dist[nei] = time + w
+                    q.append((nei, time + w))
+
+        res = max(dist.values())
+        return res if res < float('inf') else -1
