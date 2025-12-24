d5bed69ea1f98ad28d244114982e16d64ec328d8
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2025-12-23 19:11:49 -0500

    Update comments in ShortestPathFastestAlgorithm.py
    Added a note about edge relaxation in the cities array.

--- Algorithms/graphs/bellmanFord/ShortestPathFastestAlgorithm.py
+++ Algorithms/graphs/bellmanFord/ShortestPathFastestAlgorithm.py
@@ -2,6 +2,7 @@
 # Link: https://leetcode.com/problems/cheapest-flights-within-k-stops/description/
 
 # NOTE: Uses a q to bfs and a cities array to find if that path is the cheapest so far 
+# NOTE: It relaxes the edges iside the "cities" array
 # O(E + V) average, O(E*V) worst
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
