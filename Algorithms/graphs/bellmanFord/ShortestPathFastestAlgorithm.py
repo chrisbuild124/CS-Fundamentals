76d64ac6f5061cc647226c84f8313d25fdec9f2d
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2025-12-29 13:05:52 -0500

    Refactor networkDelayTime method in ShortestPathFastestAlgorithm
    Refactor Bellman-Ford algorithm implementation for clarity and efficiency.

--- Algorithms/graphs/bellmanFord/ShortestPathFastestAlgorithm.py
+++ Algorithms/graphs/bellmanFord/ShortestPathFastestAlgorithm.py
@@ -6,26 +6,23 @@
 
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        # Dijkstra: bfs: V*logE, Space: E
-            # Adj: Make: E, space: E + V
-        # Floyd Warshall: V^3 time, space: V
-        # Bellman-Ford classic: Time: V*E, space: V
-        # Bellman-Ford optimized: Average: (E + V), max: (E*V)
+        # Bellman ford, relaxes edges and takes E*V
+        # E + V (average optimized)
         INF = float('inf')
         distances = [INF]*n
         distances[k - 1] = 0
         q = deque()
-        q.append((k, 0))
+        q.append(k)
         adj = defaultdict(list)
 
         for s, e, p in times:
             adj[s].append((e, p))
 
         while q:
-            node, p = q.popleft()
+            node = q.popleft()
             for nei, edge in adj[node]:
                 if distances[node - 1] + edge < distances[nei - 1]:
                     distances[nei - 1] = distances[node - 1] + edge
-                    q.append((nei, distances[nei - 1]))
+                    q.append((nei))
 
         return -1 if INF in distances else max(distances)
