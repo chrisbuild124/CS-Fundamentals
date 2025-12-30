82da0084dbf61d0959e3252f45fe227f7addf6e1
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2025-12-29 21:14:36 -0500

    Update sortedVertexWeight.py

--- Algorithms/graphs/krustkal/sortedVertexWeight.py
+++ Algorithms/graphs/krustkal/sortedVertexWeight.py
@@ -5,42 +5,47 @@
 
 # Must be undirected, can have neg cycle
 
-class Dsu:
+class DSU:
     def __init__(self, n):
         self.size = [1]*n
-        self.parents = [i for i in range(n)]
-    
-    def find_parent(self, n):
-        if self.parents[n] != n:
-            return self.find_parent(self.parents[n])
-        return n
+        self.parent = [i for i in range(n)]
 
+    def find_parent(self, n):
+        if self.parent[n] != n:
+            self.parent[n] = self.find_parent(self.parent[n])
+        return self.parent[n]
+    
     def union(self, n1, n2):
-        pn1 = self.find_parent(n1)
-        pn2 = self.find_parent(n2)
+        pn1, pn2 = self.find_parent(n1), self.find_parent(n2)
         if pn1 == pn2:
-            return False
+            return True
         if self.size[pn1] < self.size[pn2]:
-            n1, n2, pn1, pn2 = n2, n1, pn2, pn1
-        self.parents[pn2] = pn1
+            n1, n2 = n2, n1
+            pn1, pn2 = pn2, pn1
         self.size[pn1] += self.size[pn2]
         self.size[pn2] = 0
-        return True
+        self.parent[pn2] = pn1
+        return False
 
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        def convert(i, j):
-            return len(grid[0])*i + j
-        DSU = Dsu(convert(len(grid)-1, len(grid[0])-1) + 1)
+        N = len(grid) * len(grid[0])
+        Dsu = DSU(N)
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        positions = sorted((grid[r][c], r, c) for r in range(len(grid)) for c in range(len(grid[0])))
+        res = grid[0][0]
+        heap = [(grid[0][0], 0, 0)]
+        target = (len(grid) - 1, len(grid[0]) - 1)
 
-        for t, x, y in positions:
+        def convert(i, j):
+            return i*len(grid) + j
+
+        while heap:
+            hei, x, y = heapq.heappop(heap)
+            if (x, y) == target:
+                return hei
             for ox, oy in directions:
                 nx, ny = x + ox, y + oy
                 if nx < 0 or ny < 0 or nx >= len(grid) or ny >= len(grid[0]):
                     continue
-                if t >= grid[nx][ny]:
-                    DSU.union(convert(x, y), convert(nx, ny))
-            if DSU.find_parent(0) == DSU.find_parent(convert(len(grid) - 1, len(grid) - 1)):
-                return t
+                if not Dsu.union(convert(x, y), convert(nx, ny)):
+                    heapq.heappush(heap, (max(hei, grid[nx][ny]), nx, ny))
