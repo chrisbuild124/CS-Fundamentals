ed0f950659cc617a99d4631968041b06609ae7ab
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2025-12-30 13:57:07 -0500

    Refactor DFS for topological sorting of courses
    Refactor DFS topological sort implementation for clarity and efficiency.

--- Algorithms/graphs/topoSort/dfs.py
+++ Algorithms/graphs/topoSort/dfs.py
@@ -2,37 +2,33 @@
 # DFS topological sort solution
 # https://leetcode.com/problems/course-schedule-ii/description/
 
+# NOTE: Taken directly from neetcode
+
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        indegree = [0]*numCourses
-        adj = defaultdict(list)
-        res = []
-        cycle = set() # Detects cycles
-        visit = set() # Detects if course was already taken
+        prereq = {c: [] for c in range(numCourses)}
+        for crs, pre in prerequisites:
+            prereq[crs].append(pre)
 
-        for b, a in prerequisites:
-            adj[a].append(b)
-            indegree[b] += 1
+        output = []
+        visit, cycle = set(), set()
 
-        def dfs(i):
-            if i in cycle:
+        def dfs(crs):
+            if crs in cycle:
                 return False
-            if i in visit:
+            if crs in visit:
                 return True
-            cycle.add(i)
-            visit.add(i)
-            res.append(i)
-            for nei in adj[i]:
-                indegree[nei] -= 1
-                if indegree[nei] == 0:
-                    if not dfs(nei):
-                        return False
-            cycle.remove(i)
+
+            cycle.add(crs)
+            for pre in prereq[crs]:
+                if dfs(pre) == False:
+                    return False
+            cycle.remove(crs)
+            visit.add(crs)
+            output.append(crs)
             return True
 
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                if not dfs(i):
-                    return []
-
-        return res if len(res) == numCourses else []
+        for c in range(numCourses):
+            if dfs(c) == False:
+                return []
+        return output
