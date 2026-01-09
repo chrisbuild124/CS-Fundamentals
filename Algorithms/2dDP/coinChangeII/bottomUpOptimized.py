7c37c481d745cc0369518daf33b72e17a2719d42
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2026-01-09 13:35:05 -0500

    Create bottomUpOptimized.py

---
+++ Algorithms/2dDP/coinChangeII/bottomUpOptimized.py
@@ -0,0 +1,21 @@
+# Coin change ii - bottom up solution optimized
+# Link: https://leetcode.com/problems/coin-change-ii/description/
+
+# Cannot be done in 1Ddp since the dimension is both the index and what's left
+
+# NOTE: Solution was taken directly from neetcode
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for i in range(len(coins) - 1, -1, -1):
+            nextDP = [0] * (amount + 1)
+            nextDP[0] = 1
+
+            for a in range(1, amount + 1):
+                nextDP[a] = dp[a]
+                if a - coins[i] >= 0:
+                    nextDP[a] += nextDP[a - coins[i]]
+            dp = nextDP
+        return dp[amount]
