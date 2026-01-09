7804963d1719259ef636d12283cf46a9aaccf28a
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2026-01-09 13:33:56 -0500

    Update bottomUpOptimized.py

--- Algorithms/2dDP/coinChangeII/bottomUpOptimized.py
+++ Algorithms/2dDP/coinChangeII/bottomUpOptimized.py
@@ -1,7 +1,8 @@
-#
-#
+# Coin change ii - bottom up solution optimized
+# Link: https://leetcode.com/problems/coin-change-ii/description/
 
-#
+# Cannot be done in 1Ddp since the dimension is both the index and what's left
+
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         # Bottom up dp optimized
