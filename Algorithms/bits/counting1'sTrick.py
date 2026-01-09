9c7bd36d70d6a58a24cf7266d258cae80cdd6215
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2026-01-09 14:00:50 -0500

    Create counting1'sTrick.py

---
+++ Algorithms/bits/counting1'sTrick.py
@@ -0,0 +1,14 @@
+# Counting 1's trick in a number
+# Link: https://leetcode.com/problems/number-of-1-bits/
+
+# It moves the number to the right once in binary
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+
+        while n:
+            if n & 1:
+                res += 1
+            n = n >> 1
+        return res
